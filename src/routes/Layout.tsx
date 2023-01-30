94f7fd528b2b9cabad90fa875fafdd848d038528
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-01-30 10:46:01 +0900

    feat(router): router 객체 및 RouterProvider 추가

---
+++ src/routes/Layout.tsx
@@ -0,0 +1,14 @@
+function Layout() {
+  return (
+    <div>
+      <header>
+        <div>Nav</div>
+      </header>
+      <main>
+        <div>Outlet</div>
+      </main>
+    </div>
+  );
+}
+
+export default Layout;
