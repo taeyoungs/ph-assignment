ba71ac6153aab457fc709b3f550dfc25cd304695
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-01-30 16:25:05 +0900

    chore: 임시 레포지토리 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import { QDProvider } from 'quantumic-design';
+
 import Layout from 'routes/Layout';
 import Search from 'routes/Search';
+import Repositories from 'routes/Repositories';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route index element={<Search />} />
+      <Route path="repositories" element={<Repositories />} />
     </Route>
   )
 );
---
+++ src/routes/Repositories.tsx
@@ -0,0 +1,5 @@
+function Repositories() {
+  return <div>Repository 목록</div>;
+}
+
+export default Repositories;
