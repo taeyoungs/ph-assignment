9ef428ee165ec2a3d5533b9a19090becfebbdba6
Taeyoung Jang <xoxodudwkd@gmail.com>
2023-02-01 15:38:12 +0900

    mod(Issues): Issues 컴포넌트 업데이트
    routes/Issues.tsx
    - 더미 데이터를 이용한 템플릿을 IssesList로 대체
    - 선택된 Repository 탭이 변경될 경우 query string에 존재하는
      페이지 번호를 1로 초기화
    - 등록된 Repository가 없을 경우 대응

--- src/routes/Issues.tsx
+++ src/routes/Issues.tsx
@@ -1,8 +1,11 @@
-import { useState } from 'react';
-import { Stack, Text } from 'quantumic-design';
+import { Suspense, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { Stack } from 'quantumic-design';
 import { Endpoints } from '@octokit/types';
 import { css } from '@emotion/css';
 
+import IssueList from 'components/Issues/IssueList';
+import IssueSkeleton from 'components/Issues/IssueSkeleton';
 import Tab from 'components/Issues/Tab';
 
 import { usePersistedState } from 'hooks/usePersistedState';
@@ -15,13 +18,63 @@ type Repository = Endpoints['GET /search/repositories']['response']['data']['ite
 function Issues() {
   const [enrolledRepositories] = usePersistedState<Repositories>([], LOCAL_STORAGE_KEY.REPOSITORIES);
   const [selectedRepository, setSelectedRepository] = useState(enrolledRepositories[0] ?? null);
+  const [searchParams, setSearchParams] = useSearchParams();
 
   const updateSelectedRepository = (repo: Repository) => {
     setSelectedRepository(repo);
+
+    searchParams.set('p', '1');
+    setSearchParams(searchParams);
   };
 
   if (!selectedRepository) {
-    return null;
+    return (
+      <section
+        className={css`
+          max-width: 1000px;
+          margin: 0 auto;
+        `}
+      >
+        <h1
+          className={css`
+            margin: 24px 0 12px;
+            color: ${colors.blue800};
+            font-size: 18px;
+            font-weight: 500;
+          `}
+        >
+          Issues
+        </h1>
+        <section
+          className={css`
+            padding: 24px;
+            background-color: ${colors.white};
+            box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.03);
+            border-radius: 8px;
+            margin: 24px 0;
+          `}
+        >
+          <h2
+            className={css`
+              font-size: 15px;
+              font-weight: 500;
+              color: ${colors.blue600};
+              margin: 0 0 8px;
+            `}
+          >
+            등록된 Repository가 없습니다.
+          </h2>
+          <p
+            className={css`
+              font-size: 13px;
+              color: ${colors.gray800};
+            `}
+          >
+            Repository를 검색하여 관심있는 Repository를 등록해보세요!
+          </p>
+        </section>
+      </section>
+    );
   }
 
   return (
@@ -44,116 +97,16 @@ function Issues() {
       <Stack as="ul" gap={25}>
         {enrolledRepositories.map((repo) => (
           <Tab
+            key={repo.id}
             repository={repo}
             selectedRepository={selectedRepository}
             updateSelectedRepository={updateSelectedRepository}
           />
         ))}
       </Stack>
-      <section
-        className={css`
-          padding: 24px;
-          background-color: ${colors.white};
-          box-shadow: 0px 4px 7px rgba(0, 0, 0, 0.03);
-          border-bottom-left-radius: 8px;
-          border-bottom-right-radius: 8px;
-          margin: 0 0 24px;
-          border-top: 2px solid ${colors.blue600};
-        `}
-      >
-        <ul>
-          <li
-            className={css`
-              &:not(:last-child) {
-                border-bottom: 1px solid ${colors.gray300};
-              }
-              padding: 16px 0;
-            `}
-          >
-            <Stack gap={10} direction="column">
-              <a
-                href="Apple"
-                target="_blank"
-                rel="noreferrer"
-                className={css`
-                  :hover {
-                    text-decoration: underline;
-                  }
-                `}
-              >
-                Title
-              </a>
-              <Text
-                size={13}
-                className={css`
-                  max-width: 800px;
-                  text-overflow: ellipsis;
-                  overflow: hidden;
-                  white-space: nowrap;
-                `}
-              >
-                Body
-              </Text>
-              <Stack
-                gap={10}
-                className={css`
-                  font-size: 13px;
-                  color: ${colors.gray800};
-                `}
-              >
-                <Text>Repository</Text>
-                <Text>Avatar and Name</Text>
-                <Text>Updated on -</Text>
-              </Stack>
-            </Stack>
-          </li>
-          <li
-            className={css`
-              &:not(:last-child) {
-                border-bottom: 1px solid ${colors.gray300};
-              }
-              padding: 16px 0;
-            `}
-          >
-            <Stack gap={10} direction="column">
-              <a
-                href="Apple"
-                target="_blank"
-                rel="noreferrer"
-                className={css`
-                  :hover {
-                    text-decoration: underline;
-                  }
-                `}
-              >
-                Title
-              </a>
-              <Text
-                size={13}
-                className={css`
-                  max-width: 800px;
-                  text-overflow: ellipsis;
-                  overflow: hidden;
-                  white-space: nowrap;
-                `}
-              >
-                Body
-              </Text>
-              <Stack
-                gap={10}
-                className={css`
-                  font-size: 13px;
-                  color: ${colors.gray800};
-                `}
-              >
-                <Text>Repository</Text>
-                <Text>Avatar and Name</Text>
-                <Text>Updated on -</Text>
-              </Stack>
-            </Stack>
-          </li>
-        </ul>
-      </section>
+      <Suspense fallback={<IssueSkeleton />}>
+        <IssueList repository={selectedRepository} />
+      </Suspense>
     </section>
   );
 }
